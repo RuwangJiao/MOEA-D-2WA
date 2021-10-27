2594ed8541a93b52a97e58df1fe2d1b8d1cc1bea
RuwangJiao <544945252@qq.com>
2021-10-27 21:36:05 +1300

    Update WeightGeneration.m

--- WeightGeneration.m
+++ WeightGeneration.m
@@ -19,15 +19,15 @@ function [W1, N] = WeightGeneration(N, AdN, M, thetaM)
 
 % This function is written by Ruwang Jiao
     SampleNum = 5000;
-	W1        = UniformPoint(N, M);
+    W1        = UniformPoint(N, M);
     W1        = [W1 zeros(size(W1, 1), 1)];
-	W2        = CalWeight1(thetaM, M+1, SampleNum);
+    W2        = CalWeight1(thetaM, M+1, SampleNum);
     W1        = [W1; [zeros(1, M) 1]];
 
-	while size(W1, 1) < N + AdN
-		index = find_index_with_largest_angle (W1, W2);
-		W1(size(W1, 1) + 1, :) = W2(index, :);
-		W2(index, :) = [];
+    while size(W1, 1) < N + AdN
+        index = find_index_with_largest_angle (W1, W2);
+        W1(size(W1, 1) + 1, :) = W2(index, :);
+        W2(index, :) = [];
     end	
     
     W1 = max(W1, 1e-6);
@@ -35,12 +35,12 @@ function [W1, N] = WeightGeneration(N, AdN, M, thetaM)
 end
 
 function theta = CalWeight1(thetaM, M, SampleNum)
-        theta           = rand(SampleNum, M);
-        theta           = theta./repmat(sum(theta, 2), 1, size(theta, 2));
-        SumWeiFormer    = 1 - theta(:, M);
-        SumWeiAfter     = 1 - theta(:, M).*thetaM;
-        theta(:, 1:M-1) = SumWeiAfter./SumWeiFormer.*theta(:, 1:M-1);
-        theta(:, M)     = theta(:, M).*thetaM;
+    theta           = rand(SampleNum, M);
+    theta           = theta./repmat(sum(theta, 2), 1, size(theta, 2));
+    SumWeiFormer    = 1 - theta(:, M);
+    SumWeiAfter     = 1 - theta(:, M).*thetaM;
+    theta(:, 1:M-1) = SumWeiAfter./SumWeiFormer.*theta(:, 1:M-1);
+    theta(:, M)     = theta(:, M).*thetaM;
 end
     
 function index = find_index_with_largest_angle (W1, W2)
